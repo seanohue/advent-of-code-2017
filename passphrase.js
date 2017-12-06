cbd0e95c7b9539806e65a8c92d5159beebd28184
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-12-06 18:12:48 -0500

    Did the anagram thing.

--- passphrase.js
+++ passphrase.js
@@ -1,7 +1,9 @@
-const isValid = (phrase) => !!phrase && phrase.split(' ').every((word, i, words) => words.filter(w => w === word).length === 1);
+const isValid = (phrase) => !!phrase && phrase.split(' ').every((word, i, words) => words.filter(w => isAnagram(word, w)).length <= 1);
 const numberValid = phrases => phrases.split('\n').filter(isValid).length;
+const isAnagram = (a, b) => a === b || (a.length === b.length && a.split('').sort().every((char, i) => b.split('').sort()[i] === char));
 
 module.exports = {
   isValid,
-  numberValid
+  numberValid,
+  isAnagram
 };
--- passphrase.spec.js
+++ passphrase.spec.js
@@ -16,12 +16,26 @@ let phrases =
 aa bb aa dd
 gg ff xx yy`;
 
-describe.only('Finding number of valid passphrases', function () {
+describe('Finding number of valid passphrases', function () {
   it('should return number of valid passphrases given a string w/ a phrase on each line', function () {
     expect(passphrase.numberValid(phrases)).to.equal(2);
   });
-  xit('should be able to do a lot of em', function () {
+  it('should be able to do a lot of em', function () {
     const test = require('./passphrases');
     expect(passphrase.numberValid(test)).to.equal(/*spoiler*/);
+  });
+});
+
+describe('finding anagrams', function () {
+  it('should find an anagram', function () {
+    expect(passphrase.isAnagram('cato', 'taco')).to.be.true;
+  });
+
+  it('should return true if they are the same word', function () {
+    expect(passphrase.isAnagram('same', 'same')).to.be.true;
+  });
+
+  it('should not have a false positive', function () {
+    expect(passphrase.isAnagram('potato', 'cat')).to.be.false;
   });
 });
